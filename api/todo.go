00cba6ecc31f5100f9053b1cc2971e2821bef042
codemobiles <chaiyasit.t@gmail.com>
2020-07-15 11:22:55 +0700

    ep6 - upload file

--- api/todo.go
+++ api/todo.go
@@ -1,12 +1,13 @@
 package api
 
 import (
-	// "fmt"
+	"fmt"
 	"go-todo-workshop/database"
-	// "io"
-	// "log"
+	"io"
+	"log"
 	"net/http"
-	// "os"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -61,21 +62,21 @@ func DeleteList(c *gin.Context) {
 }
 
 func Upload(c *gin.Context) {
-	// file, header, err := c.Request.FormFile("file")
-	// if err != nil {
-	// 	c.String(http.StatusBadRequest, fmt.Sprintf("file err : %s", err.Error()))
-	// 	return
-	// }
-	// filename := header.Filename
-	// out, err := os.Create("public/" + filename)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer out.Close()
-	// _, err = io.Copy(out, file)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// filepath := "http://localhost:8080/file/" + filename
-	c.JSON(http.StatusOK, gin.H{"filepath": "ok"})
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("file err : %s", err.Error()))
+		return
+	}
+	filename := header.Filename
+	out, err := os.Create("public/" + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filepath := "http://localhost:8080/file/" + filename
+	c.JSON(http.StatusOK, gin.H{"filepath": filepath})
 }
